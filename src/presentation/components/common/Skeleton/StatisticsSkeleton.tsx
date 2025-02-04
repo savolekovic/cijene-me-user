062f95d3e2b7ba70a48f87fcd0d1eb7c81bd5d6a
Savo Lekovic <savo.lekovic@amplitudo.me>
2025-02-04 09:13:54 +0100

    Enhance Skeleton components with improved styling and shimmer effect

--- src/presentation/components/common/Skeleton/StatisticsSkeleton.tsx
+++ src/presentation/components/common/Skeleton/StatisticsSkeleton.tsx
@@ -6,9 +6,9 @@ const StatisticsSkeleton: React.FC = () => {
     <div className="row">
       {[...Array(4)].map((_, index) => (
         <div key={index} className="col-md-3 col-6 mb-3">
-          <div className="price-stat-card">
-            <div className="skeleton skeleton-text mb-2" style={{ width: '70%' }}></div>
-            <div className="skeleton skeleton-title"></div>
+          <div className="price-stat-card h-100">
+            <div className="skeleton" style={{ height: '16px', width: '70%', marginBottom: '12px' }}></div>
+            <div className="skeleton" style={{ height: '32px', width: '100%' }}></div>
           </div>
         </div>
       ))}
